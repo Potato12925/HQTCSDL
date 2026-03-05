85a5051755bfae5834c222a14fedc60c4087e029
Potato12925 <bao0908235279@gmail.com>
2026-03-05 14:30:16 +0700

    Add project files.

---
+++ Controllers/DataController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HQTCSDL.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Query()
+        {
+            return View();
+        }
+        public IActionResult Report()
+        {
+            return View();
+        }
+    }
+}
